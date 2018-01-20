b8edb5a2f5d077dd6e685d98f168c7d01e36db6e
ZENZOFH <feng.zhou@tieto.com>
2018-01-20 18:34:11 +0800

    initial spring boot + mvc

---
+++ src/test/java/com/example/demo/Tester.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 单元测试继承该类即可
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DemoApplication.class)
+@Rollback
+public abstract class Tester {
+}
